3366665d7c745baf0452a58fa9497cf10e384768
tuequach <tuequach@gmail.com>
2019-04-12 18:46:16 -0500

    choosing theme for page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,13 @@
-          
-              $("#add-gif").on("click", function (event) {
+var gifs = [ "Naruto", "One Piece", "Fairy Tail", "Fullmetal Alchemist", "My Hero Academia", "Attack on Titans" ];
+var newGifs ="";
+
+var buttonGen = function () {
+
+}
+    
+    
+    
+$("#add-gif").on("click", function (event) {
                 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=V6C5daTHzk80YfiMLv6CXZxC00TBvWK9;
             
                 $.ajax({
@@ -10,5 +18,3 @@
   
   const topics = [];
   
-  
-              </script>
