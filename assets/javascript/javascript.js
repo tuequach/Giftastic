705dfe58b40863056f7596c36faa6d188363fba1
tuequach <tuequach@gmail.com>
2019-04-13 16:09:21 -0500

    adding on gif ratings and images animation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,4 @@
+//Initial Variables and Values
 var gifs = [ "Naruto", "One Piece", "Fairy Tail", "Fullmetal Alchemist", "My Hero Academia", "Attack on Titans" ];
 var newGifs ="";
 var button;
@@ -9,16 +10,33 @@ var buttonGen = function () {
     };
 }
     
-
-    
-    
-$("#add-gif").on("click", function (event) {
+// Adding in API from giphy.
+$("#add-gif").on("click", ".button", function (){
+                var blank = $(this).attr("data");
                 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=V6C5daTHzk80YfiMLv6CXZxC00TBvWK9";
             
                 $.ajax({
                   url: queryURL,
                   method: "GET"
-               });
+               })
+               .done(function(response){
+                  console.log(response);
+
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++){
+                  var gifsDiv = $("<div>");
+
+                  //Displaying ratings for each gif
+                  var p = $("<p>");
+                  p.text(results[i].rating);
+                  var p = $("<p>").text("Rating: " + results[i].rating);
+
+                  gifsImage.attr("src", results[i].images.fixedheightstill.url);
+                  gifsImage.attr("data-still", results[i].images.fixedheightstill.url);
+                  gifsImage.attr("data-animate", results[i].images.fixedheightstill.url);
+                  gifsImage.attr("data-state", results[i].images.fixedheightstill.url);
+
                 });
   
   const topics = [];
