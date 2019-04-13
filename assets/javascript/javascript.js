211a6e43c4064d101cbd713b676dd3a38a01ccb4
tuequach <tuequach@gmail.com>
2019-04-12 19:25:24 -0500

    adding on for loop for creating new button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,19 @@
 var gifs = [ "Naruto", "One Piece", "Fairy Tail", "Fullmetal Alchemist", "My Hero Academia", "Attack on Titans" ];
 var newGifs ="";
+var button;
 
 var buttonGen = function () {
-
+    $("#add-gif").empty();
+    for (var i= 0; i< gifs.length; i++){
+        $("#add-gifs").append(button);
+    };
 }
     
+
     
     
 $("#add-gif").on("click", function (event) {
-                var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=V6C5daTHzk80YfiMLv6CXZxC00TBvWK9;
+                var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=V6C5daTHzk80YfiMLv6CXZxC00TBvWK9";
             
                 $.ajax({
                   url: queryURL,
